47626056b11aafd16eebc9940ee35e8987ae85a0
ignaciohl <ihlopez18@gmail.com>
2023-05-14 17:33:58 +0200

    Database y Ejercicio1

--- main.py
+++ main.py
@@ -1,16 +1,106 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import sqlite3
+import json
+import pandas as pd
+from flask import *
 
 
-# Press the green button in the gutter to run the script.
+#Conectar la bd creada con sqlite
+con= sqlite3.connect('practica.db')
+
+#Leer datos proporcionados (fase de extracción)
+df_alertas= pd.read_csv('alerts.csv')
+d= open("devices.json")
+dispositivos= json.load(d)
+
+
+cur=con.cursor()
+
+#Diseñar las tablas
+cur.execute("CREATE TABLE IF NOT EXISTS responsable(nombre TEXT PRIMARY_KEY, telefono TEXT, rol TEXT)")
+cur.execute("CREATE TABLE IF NOT EXISTS analisis(id TEXT PRIMARY_KEY, puertos_abiertos TEXT, numPuertosAbiertos TEXT, servicios TEXT, servicios_inseguros TEXT, vulnerabilidades_detectadas INTEGER)")
+cur.execute("CREATE TABLE IF NOT EXISTS devices(id TEXT, ip TEXT, localizacion TEXT,responsable_id TEXT, analisis_id TEXT, FOREIGN KEY(responsable_id) REFERENCES responsable(nombre), FOREIGN KEY(analisis_id) REFERENCES analisis(id))")
+cur.execute("CREATE TABLE IF NOT EXISTS alerts(timestamp TEXT, sid TEXT, msg TEXT,clasificacion TEXT, prioridad TEXT, protocolo TEXT, origen TEXT, destino TEXT, puerto TEXT  )")
+
+#Datos tabla alerts
+df_alertas.to_sql('alerts',con,if_exists='replace',index=False)
+
+#Datos tabla responsable
+cur.execute("INSERT INTO responsable VALUES ('admin', '656445552','Administracion de sistemas')")
+cur.execute("INSERT INTO responsable VALUES ('Paco Garcia', '640220120','Direccion')")
+cur.execute("INSERT INTO responsable VALUES ('Luis Sanchez', 'None','Desarrollador')")
+cur.execute("INSERT INTO responsable VALUES ('admin', '656445552','Administracion de sistemas')")
+cur.execute("INSERT INTO responsable VALUES ('admiin', 'None','None')")
+cur.execute("INSERT INTO responsable VALUES ('admin', '656445552','Administracion de sistemas')")
+cur.execute("INSERT INTO responsable VALUES ('admin','656445552','Administracion de sistemas')")
+
+#Datos tabla analisis
+cur.execute("INSERT INTO analisis VALUES(1, '80/TCP, 443/TCP, 3306/TCP, 40000/UDP', 4, 3, 0, 15)")
+cur.execute("INSERT INTO analisis VALUES(2, 'None', 0, 0, 0, 4)")
+cur.execute("INSERT INTO analisis VALUES(3, '1194/UDP, 8080/TCP,8080/UDP, 40000/UDP',4, 1, 1, 52)")
+cur.execute("INSERT INTO analisis VALUES(4, '443/UDP, 80/TCP',2, 1, 0,3)")
+cur.execute("INSERT INTO analisis VALUES(5, '80/TCP, 67/UDP, 68/UDP', 3, 2, 2, 12)")
+cur.execute("INSERT INTO analisis VALUES(6, '8080/TCP, 3306/TCP, 3306/UDP', 3,2, 0, 2)")
+cur.execute("INSERT INTO analisis VALUES(7, '80/TCP, 443/TCP, 9200/TCP, 9300/TCP, 5601/TCP', 5,3, 2, 21)")
+
+#Datos tabla devices
+cur.execute("INSERT INTO devices VALUES('web', '172.18.0.0', 'None','admin', 1)")
+cur.execute("INSERT INTO devices VALUES('paco_pc', '172.17.0.0', 'Barcelona','Paco Garcia', 2)")
+cur.execute("INSERT INTO devices VALUES('luis_pc', '172.19.0.0', 'Madrid','Luis Sanchez', 3)")
+cur.execute("INSERT INTO devices VALUES('router1', '172.1.0.0', 'None','admin', 4)")
+cur.execute("INSERT INTO devices VALUES('dhcp_server', '172.1.0.1', 'Madrid','admiin', 5)")
+cur.execute("INSERT INTO devices VALUES('mysql_db', '172.18.0.1', 'None','admin', 6)")
+cur.execute("INSERT INTO devices VALUES('ELK', '172.18.0.2', 'None','admin', 7)")
+
+
+app= Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/ip")
+def ip():
+    con = sqlite3.connect("practica.db")
+    cur = con.cursor()
+    num=request.args.get("number_ips")
+    cur.execute("SELECT origen, COUNT(*) as num FROM alerts WHERE prioridad = 1 GROUP BY origen ORDER BY num DESC LIMIT ?",(num,))
+    resultados=cur.fetchall()
+    return render_template("ip.html",num=num,resultados=resultados)
+
+@app.route("/devices")
+def devices():
+    con = sqlite3.connect("practica.db")
+    cur = con.cursor()
+    num = request.args.get("number_devices")
+    cur.execute("SELECT d.id, SUM(a.servicios_inseguros + a.vulnerabilidades_detectadas) as total FROM devices d JOIN analisis a ON d.analisis_id = a.id GROUP BY d.id ORDER BY total DESC LIMIT ?", (num,))
+    resultados = cur.fetchall()
+    return render_template("devices.html", num=num, resultados=resultados)
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
